ab22177594cb8c9340311e120479457522bb6375
TopotTut <n.kopylov@neuromed.tech>
2025-03-17 17:32:26 +0300

    геокодер Назад

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 ADMIN_IDS = os.getenv("ADMIN_IDS")
+OPEN_CAGE_API_KEY = os.getenv("OPEN_CAGE_API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
